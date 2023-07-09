5a2fcebe55b1bfd619b012cdd5b562cac66d7d2e
Kevin Martínez Palmieri <martinezpke26@gmail.com>
2023-07-08 21:06:44 -0500

    Update vite.config.js

--- pages-404/vite.config.js
+++ pages-404/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/martinezpke/404-not-found-master',
+  base: 'https://github.com/martinezpke/404-not-found-master'
 })
